8afad7b77e4a46472e220305768c9986e5eeb0cf
bemac-honda <tomoya-honda@bemac-jp.com>
2021-03-05 15:42:35 +0900

    Initial commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import plotly.express as px
 import pandas as pd
 
 whc = pd.read_csv(
-    'c:/Users/t_honda/Desktop/Dash/data/whc-sites-2019.csv')
+    'c:/Users/t_honda/Desktop/world-heritage-cite/data/whc-sites-2019.csv')
 r = whc['region_en'].unique()
 regions = np.append('All over the world', r)
 categories = whc['category'].unique()
