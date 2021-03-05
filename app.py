681963d998d82d0f222b81a04e8d51359d7a3591
bemac-honda <tomoya-honda@bemac-jp.com>
2021-03-05 15:24:20 +0900

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
+import os
+
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
@@ -8,8 +10,6 @@ from dash.dependencies import Input, Output
 import numpy as np
 import plotly.express as px
 import pandas as pd
-
-import os
 
 whc = pd.read_csv(
     'c:/Users/t_honda/Desktop/Dash/data/whc-sites-2019.csv')
